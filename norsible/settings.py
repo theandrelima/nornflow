ae04c07fc4e5c95b40c34eedeea213d0dc788333
André Lima <39955130+theandrelima@users.noreply.github.com>
2025-02-05 12:22:52 +0000

    in the bining-ning....

---
+++ norsible/settings.py
@@ -0,0 +1,18 @@
+class NorsibleSettings:
+    """
+    This class is used to hold norsible settings parameters, which have sensible
+    defaults that can be overwritten by the environment variables.
+    """
+    pass
+
+
+GLOBAL_CONFIGS = None
+
+
+def get_config():
+    global GLOBAL_CONFIGS
+
+    if GLOBAL_CONFIGS is None:
+        GLOBAL_CONFIGS = NorsibleSettings()
+
+    return GLOBAL_CONFIGS
