adf42dbf7403ea5bc4feaa8c61fa00006cd56c68
André Lima <39955130+theandrelima@users.noreply.github.com>
2025-02-06 12:00:00 +0000

    improvements to NornFlowSettings class

--- nornflow/settings.py
+++ nornflow/settings.py
@@ -1,40 +1,80 @@
 import os
+from typing import Dict, List, Any
 from collections import defaultdict
 from utils import read_yaml_file
-from constants import TRUTHY
 
 class NornFlowSettings:
     """
     This class is used to store NornFlow settings for access during runtime.
     
     For initialization, it requires the location of a YAML file that holds the settings.
-    
-    By default, it expects a file named `nornflow.yaml` to be in the same directory
-    from where the Python interpreter is run. 
-    
-    The user can override this default by setting the `NORNFLOW_CONFIG_FILE` environment variable.
-    
-    To allow for ease of extensibility and customizations, NornFlow was
-    designed with the following principles in mind:
-        - a minimal amount of required settings, namely:
+    This will be determined with the following order of preference:
+        - through an environment variable named `NORNFLOW_CONFIG_FILE`.
+        - through the `config_file` argument.
+        - a default file named 'nornflow.yaml' is assumed to exist.
+        
+    To allow for extensibility and customizations, NornFlow was designed with the following
+    principles in mind:
+        1 - NornFlow settings and Nornir settings are kept separate, hence the need for a 
+           `nornir_config_file` setting in the NornFlow YAML settings file (see '2' below).
+        
+        2 - a minimal set of REQUIRED settings:
             - `nornir_config_file`
             - `tasks`
-        - users can add more settings according to their needs to the `nornflow.yaml` config file
-        - settings can be accessed as attributes of a NornFlowSettings object:
-          NornFlowSettings().tasks # returns the 'tasks' setting
-        - non-existing settings will return None:
-          NornFlowSettings().non_existing_setting  # returns None
+
+        3 - a minimal set of OPTIONAL settings that can also be passed explicitly to the Class
+            initializer as keyword arguments.
+            - `dry_run`: defaults to False
+            - `parallel_execution` defaults to True
+
+        4 - there's no fixed set of "acceptable settings". Users can add more settings if 
+            they want to extend NornFlow to support custom use-cases.
+
+        5 - settings can be accessed as attributes of a NornFlowSettings object:
+            NornFlowSettings().tasks # returns the 'tasks' setting
+
+        6 - trying to access settings that were not informe in the YAMLS nor passed as a keyword
+            args will simply return None:
+            NornFlowSettings().non_existing_setting  # returns None
     """
-    def __init__(self, config_file: str = "nornflow.yaml", dry_run: bool = False):
+    
+    MANDATORY_SETTINGS = ["nornir_config_file", "tasks"]
+    OPTIONAL_SETTINGS = ["dry_run", "parallel_execution"]
+    
+    def __init__(self, config_file: str = "nornflow.yaml", **kwargs):
         # Use environment variable to override config file path if set
         self.config_file = os.getenv("NORNFLOW_CONFIG_FILE", config_file)
         self._load_config()
-        
-        self.dry_run = self.dry_run or dry_run
-
+        self._check_mandatory_settings()
+        self._set_optional_settings(**kwargs)
+    
     def _load_config(self):
         config_data = read_yaml_file(self.config_file)
         self.config = defaultdict(lambda: None, config_data)
+    
+    def _check_mandatory_settings(self) -> None:
+        """
+        Check if all mandatory settings are present and not empty in the configuration.
+    
+        Raises:
+            ValueError: If a mandatory setting is missing or empty.
+        """
+        for setting in self.MANDATORY_SETTINGS:
+            if setting not in self.config:
+                raise ValueError(f"Missing mandatory setting: {setting}.")
+            if not self.config[setting]:
+                raise ValueError(f"Setting {setting} can't be empty.")
+    
+    def _set_optional_settings(self, **kwargs: Any) -> None:
+        """
+        Set optional settings from kwargs or default to existing attributes.
+        This enforces preference for optional settings passed as keyword arguments.
+
+        Args:
+            **kwargs (Any): Keyword arguments containing optional settings.
+        """
+        for setting in self.OPTIONAL_SETTINGS:
+            setattr(self, setting, kwargs.get(setting, getattr(self, setting)))
         
     @property
     def nornir_configs(self):
