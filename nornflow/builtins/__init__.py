e27569b657411ed4e9b7827dc7b8e7f1ed6bd72e
André Lima <39955130+theandrelima@users.noreply.github.com>
2025-10-19 20:52:27 +0100

    feat(builtins): export SetPrintOutputHook and SetToHook
    - Add new hooks to __all__ exports
    - Import hooks from nornflow.builtins.hooks module

--- nornflow/builtins/__init__.py
+++ nornflow/builtins/__init__.py
@@ -6,9 +6,12 @@ This package contains the default filters and processors included with NornFlow.
 
 from nornflow.builtins.filters import groups, hosts
 from nornflow.builtins.processors import DefaultNornFlowProcessor
+from nornflow.builtins.hooks import SetPrintOutputHook, SetToHook
 
 __all__ = [
     "DefaultNornFlowProcessor",
     "groups",
     "hosts",
+    "SetPrintOutputHook",
+    "SetToHook"
 ]
